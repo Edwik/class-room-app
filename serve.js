8c36dc9b6456ab822ea83411d80c77a96cafd779
Edwin Anaya <eanaya@lean-tech.io>
2020-03-25 00:22:47 -0500

    commit 4

--- serve.js
+++ serve.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const path = require('path')
-var secure = require('ssl-express-www');
 const app = express()
 
 app.use(express.static(path.join(__dirname, 'build')))
-app.use(secure);
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'))
