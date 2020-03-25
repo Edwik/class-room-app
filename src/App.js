296a3144ea870e8ee7d947713775c5c8c7d6cb72
Edwin Anaya <eanaya@lean-tech.io>
2020-03-25 00:49:11 -0500

    commit 6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import $ from "jquery";
 import './App.css';
 
 function App() {
+
+  useEffect( ()=>{
+    $("#doc").attr("src",!process.env.DOC ? 'https://docs.google.com/presentation/d/1nIUvPah0WQEEVwmTGQ1xrUDt4y2ZtYmC/edit' : process.env.DOC);
+    $("#youtube").attr("src",!process.env.YOUTUBE ? 'https://www.youtube.com/embed/4ysArLvNHMg?rel=0&amp;autoplay=1' : process.env.YOUTUBE+'?rel=0&amp;autoplay=1');
+    $("#chat").attr("src",!process.env.CHAT ? 'https://www.youtube.com/live_chat?v=4ysArLvNHMg&embed_domain=class-room-app.herokuapp.com' : process.env.CHAT);
+  },[])
+
   return (
     <div className="App" style={{display: 'flex', flexDirection: 'row', height:"100vh"}}>
 
       <div className="Col1" style={{backgroundColor: 'grey', width: '75%', height: '100%'}}>
-        <iframe title="DOC" width="100%" height="100%" src={process.env.DOC}></iframe>
+        <iframe id="doc" title="DOC" width="100%" height="100%" src=""></iframe>
       </div>
 
       <div className="Col2" style={{width: '25%', display: 'flex', flexDirection: 'column'}}>
-        <iframe title="YOUTUBE" width="100%" height="300" src={process.env.ID} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
-        <iframe  title="CHAT"allowfullscreen="" frameborder="0" height="100%" src={process.env.CHAT} width="100%"></iframe>
+        <iframe id="youtube" title="YOUTUBE" width="100%" height="300" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
+        <iframe  id="chat" title="CHAT"allowfullscreen="" frameborder="0" height="100%" width="100%"></iframe>
       </div>
 
     </div>
