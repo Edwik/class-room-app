6738fc17848172a02014f2d5e2b8f1f2f7f77346
Edwin Anaya <eanaya@lean-tech.io>
2020-03-25 00:14:34 -0500

    commin2

--- serve.js
+++ serve.js
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path')
 const app = express()
 
-app.use(express.static(path.join(__dirname, 'web-build')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'web-build', 'index.html'))
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
 app.listen(process.env.PORT || 9001)
