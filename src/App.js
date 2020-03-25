144d67e40d1c39ef6ff3d3c324e11adea1b7339d
Edwin Anaya <eanaya@lean-tech.io>
2020-03-25 01:35:35 -0500

    ccc

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 function App() {
 
   useEffect( ()=>{
-    $("#doc").attr("src",!process.env.DOC ? 'https://docs.google.com/presentation/d/1nIUvPah0WQEEVwmTGQ1xrUDt4y2ZtYmC/edit' : process.env.DOC);
-    $("#youtube").attr("src",!process.env.YOUTUBE ? 'https://www.youtube.com/embed/4ysArLvNHMg?rel=0&amp;autoplay=1' : process.env.YOUTUBE+'?rel=0&amp;autoplay=1');
+    $("#doc").attr("src",!process.env.DOC ? 'https://docs.google.com/document/d/1eDRx91Pcub4GODmj403Jm7n03dlSRnWdwpUo8O-sDdk/edit' : process.env.DOC);
+    $("#youtube").attr("src",!process.env.YOUTUBE ? 'https://www.youtube.com/embed/LU0kvA6Cmeo;autoplay=1' : process.env.YOUTUBE+'?rel=0&amp;autoplay=1');
     $("#chat").attr("src",!process.env.CHAT ? 'https://www.youtube.com/live_chat?v=4ysArLvNHMg&embed_domain=class-room-app.herokuapp.com' : process.env.CHAT);
   },[])
 
